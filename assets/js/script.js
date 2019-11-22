3fdb6db90a4520b17813b955c1e5821ed14e2668
EmilieLamps <emilie.lamps@gmail.com>
2019-11-22 14:29:28 +0100

    Premier envoi

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var c = document.getElementById("mon_canvas");
+var ctx = c.getContext("2d");
+
+//Toit de la maison
+
+
+// Forme de la maison
+
+
+//  Fenêtre de gauche
+
+
+// Fenêtre de droite
+
+
+// Porte
